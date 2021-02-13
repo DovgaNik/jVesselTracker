36b918bde55383e2c8b615c57e609edaaa243331
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-02-13 20:20:50 +0200

    Added GSON to the project

--- src/jvesseltracker/JVesselTracker.java
+++ src/jvesseltracker/JVesselTracker.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 public class JVesselTracker {
 
-	private static final String USER_AGENT = "Mozilla/5.0";
+	private static final String agent = "Mozilla/5.0";
 
-	private static final String GET_URL = "https://services.marinetraffic.com/api/exportvessel/v:5/dbc7e47f9ead3b31f4602a1720d6219e489d27a8/timespan:100/mmsi:246896000";
+	private static final String url = "https://services.marinetraffic.com/api/exportvessel/v:5/dbc7e47f9ead3b31f4602a1720d6219e489d27a8/timespan:100/mmsi:246896000/protocol:json";
 
 	public static void main(String[] args) throws IOException, Exception {
 
 
-		JWebRequests jWeb = new JWebRequests(USER_AGENT, GET_URL);
+		JWebRequests jWeb = new JWebRequests(agent, url);
 
 		System.out.println(jWeb.sendGET());
 		
