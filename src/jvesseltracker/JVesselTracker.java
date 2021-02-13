8bff911362b356c33bf456cb873d6812ee33fce3
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-02-13 17:55:41 +0200

    Moved get request to dedicated class with its own constructor TODO: to add deconstructor

--- src/jvesseltracker/JVesselTracker.java
+++ src/jvesseltracker/JVesselTracker.java
@@ -12,38 +12,13 @@ public class JVesselTracker {
 
 	private static final String GET_URL = "https://services.marinetraffic.com/api/exportvessel/v:5/dbc7e47f9ead3b31f4602a1720d6219e489d27a8/timespan:100/mmsi:246896000";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, Exception {
 
-		URL link = new URL(GET_URL);
-		System.out.println(sendGET(link));
+
+		JWebRequests jWeb = new JWebRequests(USER_AGENT, GET_URL);
+
+		System.out.println(jWeb.sendGET());
 		
-	}
-
-	private static String sendGET(URL URL) throws IOException {
-		
-		HttpURLConnection con = (HttpURLConnection) URL.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			
-			in.close();
-
-			// print result
-			return (response.toString());
-		} else {
-			return ("GET request not worked");
-		}
-
 	}
 	
 }
--- src/jvesseltracker/JWebRequests.java
+++ src/jvesseltracker/JWebRequests.java
@@ -1,5 +1,81 @@
 package jvesseltracker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class JWebRequests {
+
+	static HttpURLConnection connection;
+	String link;
 	
+	JWebRequests(final String userAgent, final String getPostURL) throws MalformedURLException, IOException{
+		
+		URL URL = new URL(getPostURL);
+		link = getPostURL;
+		connection = (HttpURLConnection) URL.openConnection();
+		connection.setRequestProperty("User-Agent", userAgent);
+		
+	}
+
+	public static String sendGET() throws IOException {
+		connection.setRequestMethod("GET");
+		int responseCode = connection.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			return response.toString();
+		} else {
+			System.out.println("GET request not worked");
+			return null;
+		}
+
+	}
+
+	private static void sendPOST(final String postParams) throws IOException {
+		connection.setRequestMethod("POST");
+
+		// For POST only - START
+		connection.setDoOutput(true);
+		OutputStream os = connection.getOutputStream();
+		os.write(postParams.getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = connection.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+
+		if (responseCode == HttpURLConnection.HTTP_OK) { //success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("POST request not worked");
+		}
+	}
+
 }
