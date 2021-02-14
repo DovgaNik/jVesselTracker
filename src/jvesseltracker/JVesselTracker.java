c7f19f8f059c6adb7d92603326fe59b8a41ec503
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-02-14 19:11:59 +0200

    Changed the way of URL forming Added dist folder to the .gitignore file

--- src/jvesseltracker/JVesselTracker.java
+++ src/jvesseltracker/JVesselTracker.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 public class JVesselTracker {
 
-	private static final String agent = "Mozilla/5.0";
+	private static final String agent = "Mozilla/5.0";//User agent by default (recommended)
 
-	private static final String url = "https://services.marinetraffic.com/api/exportvessel/v:5/dbc7e47f9ead3b31f4602a1720d6219e489d27a8/timespan:100/mmsi:246896000/protocol:json";
+	private static final String shipid = "dbc7e47f9ead3b31f4602a1720d6219e489d27a8";//shipid on marinetraffic
+	private static final String mmsi = "246896000"; //mmsi
+	private static final String timespan = "10"; //timespan of the position
+	
+	private static final String url = "https://services.marinetraffic.com/api/exportvessel/v:5/" + shipid + "/timespan:" + timespan + "/mmsi:" + mmsi + "/protocol:json";// Addres to the API of MarineTraffic
 
 	public static void main(String[] args) throws IOException {
-
 
 		JWebRequests jWeb = new JWebRequests(agent, url);
 	
@@ -18,20 +21,20 @@ public class JVesselTracker {
 
 		parsedJson parser = new parsedJson(json);
 		
-		System.out.println("course" + parser.course);
-		System.out.println("dataSource" + parser.dataSource);
-		System.out.println("day" + parser.day);
-		System.out.println("heading" + parser.heading);
-		System.out.println("hour" + parser.hour);
-		System.out.println("lat" + parser.lat);
-		System.out.println("lat" + parser.lon);
-		System.out.println("minute" + parser.minute);
-		System.out.println("mmsi" + parser.mmsi);
-		System.out.println("month" + parser.month);
-		System.out.println("second" + parser.second);
-		System.out.println("speed" + parser.speed);
-		System.out.println("status" + parser.status);
-		System.out.println("year" + parser.year);
+		System.out.println("course " + parser.course);
+		System.out.println("dataSource " + parser.dataSource);
+		System.out.println("day " + parser.day);
+		System.out.println("heading " + parser.heading);
+		System.out.println("hour " + parser.hour);
+		System.out.println("lat " + parser.lat);
+		System.out.println("lon " + parser.lon);
+		System.out.println("minute " + parser.minute);
+		System.out.println("mmsi " + parser.mmsi);
+		System.out.println("month " + parser.month);
+		System.out.println("second " + parser.second);
+		System.out.println("speed " + parser.speed);
+		System.out.println("status " + parser.status);
+		System.out.println("year " + parser.year);
 			
 	}
 	
