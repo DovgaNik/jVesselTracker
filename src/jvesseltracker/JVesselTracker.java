01b59c4aaec8df06a9d097a9e32db180aa9806dd
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-02-19 21:30:23 +0200

    Fixed bug with timespan

--- src/jvesseltracker/JVesselTracker.java
+++ src/jvesseltracker/JVesselTracker.java
@@ -8,7 +8,7 @@ public class JVesselTracker {
 
 	private static final String shipid = "dbc7e47f9ead3b31f4602a1720d6219e489d27a8";//shipid on marinetraffic
 	private static final String mmsi = "246896000"; //mmsi
-	private static final String timespan = "10"; //timespan of the position
+	private static byte timespan = 10; //timespan of the position
 	
 	private static final String url = "https://services.marinetraffic.com/api/exportvessel/v:5/" + shipid + "/timespan:" + timespan + "/mmsi:" + mmsi + "/protocol:json";// Addres to the API of MarineTraffic
 
@@ -18,24 +18,31 @@ public class JVesselTracker {
 	
 		String json = jWeb.sendGET();
 		System.out.println(json);
-
-		parsedJson parser = new parsedJson(json);
 		
-		System.out.println("course " + parser.course);
-		System.out.println("dataSource " + parser.dataSource);
-		System.out.println("day " + parser.day);
-		System.out.println("heading " + parser.heading);
-		System.out.println("hour " + parser.hour);
-		System.out.println("lat " + parser.lat);
-		System.out.println("lon " + parser.lon);
-		System.out.println("minute " + parser.minute);
-		System.out.println("mmsi " + parser.mmsi);
-		System.out.println("month " + parser.month);
-		System.out.println("second " + parser.second);
-		System.out.println("speed " + parser.speed);
-		System.out.println("status " + parser.status);
-		System.out.println("year " + parser.year);
+		System.out.println(json.length());
+		
+		if(json.length() == 3){
+			System.out.println("Fuck");
+		}else{
+		
+			parsedJson parser = new parsedJson(json);
+
+			System.out.println("course " + parser.course);
+			System.out.println("dataSource " + parser.dataSource);
+			System.out.println("day " + parser.day);
+			System.out.println("heading " + parser.heading);
+			System.out.println("hour " + parser.hour);
+			System.out.println("lat " + parser.lat);
+			System.out.println("lon " + parser.lon);
+			System.out.println("minute " + parser.minute);
+			System.out.println("mmsi " + parser.mmsi);
+			System.out.println("month " + parser.month);
+			System.out.println("second " + parser.second);
+			System.out.println("speed " + parser.speed);
+			System.out.println("status " + parser.status);
+			System.out.println("year " + parser.year);
 			
+		}
 	}
 	
 }
