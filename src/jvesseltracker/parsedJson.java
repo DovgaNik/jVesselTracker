f8f9e83d362d2e0462f2461f64e380043739763d
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-02-19 19:36:26 +0200

    Added many comments to the parser

--- src/jvesseltracker/parsedJson.java
+++ src/jvesseltracker/parsedJson.java
@@ -1,7 +1,7 @@
 package jvesseltracker;
 
 public class parsedJson {
-	String 
+	String //Creating public string to store all parsed information
 		mmsi, 
 		lat, 
 		lon, 
@@ -17,38 +17,40 @@ public class parsedJson {
 		second, 
 		dataSource;
 	
-	protected String 
+	protected String // Creating variables, available only in this class and used for storing temporary information
 		json, 
 		temp = "", 
 		dateTime[],
 		date[], 
 		time[];
 	
-	parsedJson(String jsonToParse){
-		json = jsonToParse;
+	parsedJson(String jsonToParse){ // Constructor of the class
+		json = jsonToParse; // Writes variable, given by driver code to a protected variable
 		
-		parse();
+		parse(); //Calling parsing function
 	}
 	
 	private void parse(){
+		// <editor-fold defaultstate="collapsed" desc="deleting all unwanted characters in downloaded jason file">
 		for (int i = 0; i < json.length(); i++) {
 			char charAt = json.charAt(i);
 			if (charAt != '[' & json.charAt(i) != ']' & json.charAt(i) != '"') {
 				temp += charAt;
 			}
 		}
+		// </editor-fold>
 		
-		System.out.println(temp);
+		System.out.println(temp); // Output of temporary string with deleted characters
 		
-		String[] jsonInfo = temp.split(",");
+		String[] jsonInfo = temp.split(","); // Splitting all parametres of the vessel
 		
 		for (int i = 0; i < jsonInfo.length; i++) {
 			System.out.println(i + ". " +jsonInfo[i]);
 		}
 		
-		dateTime = jsonInfo[7].split("T");
-		date = dateTime[0].split("-");
-		time = dateTime[1].split(":");
+		dateTime = jsonInfo[7].split("T");// Parsing date and time to different indexes in an array
+		date = dateTime[0].split("-");// Parsing date
+		time = dateTime[1].split(":");// parsing time
 		
 		mmsi = jsonInfo[0];
 		lat = jsonInfo[1];
