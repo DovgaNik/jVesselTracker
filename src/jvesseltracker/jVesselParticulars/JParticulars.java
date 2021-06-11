fa5953f7503b6a5f833f3280efd209a3bbd9d779
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-06-12 01:36:45 +0300

    Added code to get photo link

---
+++ src/jvesseltracker/jVesselParticulars/JParticulars.java
@@ -0,0 +1,37 @@
+package jvesseltracker.jVesselParticulars;
+
+import java.io.IOException;
+import jvesseltracker.JWebRequests;
+
+public class JParticulars {
+    
+    public static void get(String mmsi, String apikey, String agent) throws IOException{
+    
+        String url = "https://services.marinetraffic.com/api/exportvesselphoto/" + apikey + "/vessel_id:" + mmsi + "/protocol:json";
+        
+        System.out.println(url);
+        
+        jvesseltracker.JWebRequests jWeb = new jvesseltracker.JWebRequests(agent, url);
+        String json = jWeb.sendGET();
+        
+        System.out.println(json);
+        
+        String temp = "";
+        
+        // <editor-fold defaultstate="collapsed" desc="deleting all unwanted characters in downloaded jason file">
+            for (int i = 0; i < json.length(); i++) {
+		char charAt = json.charAt(i);
+		if (charAt != '[' & json.charAt(i) != ']' & json.charAt(i) != '"' & json.charAt(i) != '\\') {
+                    temp += charAt;
+                }
+            }
+	// </editor-fold>
+        
+        System.out.println(temp);
+        
+        jvesseltracker.JWebRequests jWebTemp = new jvesseltracker.JWebRequests(agent, temp);
+        String photohtml = jWebTemp.sendGET();
+        System.out.println(photohtml);
+    }
+            
+}
