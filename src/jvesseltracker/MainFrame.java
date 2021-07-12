3004d1ae6c5393230ce81a3eb2f88eac547b2517
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-07-12 16:45:16 +0300

    Little refactor

--- src/jvesseltracker/MainFrame.java
+++ src/jvesseltracker/MainFrame.java
@@ -34,6 +34,9 @@ public class MainFrame extends javax.swing.JFrame {
         jLabelVesselType4 = new javax.swing.JLabel();
         jLabelVesselCoordinates4 = new javax.swing.JLabel();
         jPaneShip5 = new javax.swing.JPanel();
+        jLabelVesselName5 = new javax.swing.JLabel();
+        jLabelVesselType5 = new javax.swing.JLabel();
+        jLabelVesselCoordinates5 = new javax.swing.JLabel();
         jPanelShipAnim1 = new javax.swing.JPanel();
         jPanelShipAnim2 = new javax.swing.JPanel();
         jPanelShipAnim3 = new javax.swing.JPanel();
@@ -266,15 +269,37 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
+        jLabelVesselName5.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
+        jLabelVesselName5.setMaximumSize(new java.awt.Dimension(280, 42));
+
+        jLabelVesselCoordinates5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabelVesselCoordinates5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout jPaneShip5Layout = new javax.swing.GroupLayout(jPaneShip5);
         jPaneShip5.setLayout(jPaneShip5Layout);
         jPaneShip5Layout.setHorizontalGroup(
             jPaneShip5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPaneShip5Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPaneShip5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPaneShip5Layout.createSequentialGroup()
+                        .addComponent(jLabelVesselName5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPaneShip5Layout.createSequentialGroup()
+                        .addComponent(jLabelVesselType5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                        .addComponent(jLabelVesselCoordinates5, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
         jPaneShip5Layout.setVerticalGroup(
             jPaneShip5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPaneShip5Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabelVesselName5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPaneShip5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabelVesselType5, javax.swing.GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
+                    .addComponent(jLabelVesselCoordinates5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         jPanelSidebar.add(jPaneShip5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 455, 296, 100));
@@ -478,14 +503,17 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabelVesselCoordinates2;
     private javax.swing.JLabel jLabelVesselCoordinates3;
     private javax.swing.JLabel jLabelVesselCoordinates4;
+    private javax.swing.JLabel jLabelVesselCoordinates5;
     private javax.swing.JLabel jLabelVesselName1;
     private javax.swing.JLabel jLabelVesselName2;
     private javax.swing.JLabel jLabelVesselName3;
     private javax.swing.JLabel jLabelVesselName4;
+    private javax.swing.JLabel jLabelVesselName5;
     private javax.swing.JLabel jLabelVesselType1;
     private javax.swing.JLabel jLabelVesselType2;
     private javax.swing.JLabel jLabelVesselType3;
     private javax.swing.JLabel jLabelVesselType4;
+    private javax.swing.JLabel jLabelVesselType5;
     private javax.swing.JPanel jPaneShip5;
     private javax.swing.JPanel jPaneShipl2;
     private javax.swing.JPanel jPaneShipl4;
