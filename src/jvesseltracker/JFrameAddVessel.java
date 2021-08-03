dc49d39696e2183d47a7644b1255b859645e24fe
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-08-03 22:38:01 +0300

    Changed some params on the JFrameAddVessel

--- src/jvesseltracker/JFrameAddVessel.java
+++ src/jvesseltracker/JFrameAddVessel.java
@@ -15,7 +15,11 @@ public class JFrameAddVessel extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Add Vessel");
+        setLocationByPlatform(true);
+        setUndecorated(true);
+        setType(java.awt.Window.Type.UTILITY);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
