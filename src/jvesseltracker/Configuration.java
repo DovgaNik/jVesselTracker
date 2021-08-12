2cc19c55ed1230d2f51a54e860943ec2d18862ce
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-08-12 16:35:41 +0300

    Added code for reading configuration from file

--- src/jvesseltracker/Configuration.java
+++ src/jvesseltracker/Configuration.java
@@ -1,12 +1,34 @@
 package jvesseltracker;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Configuration {
     
     public String apiPS07, apiVD01;
     
-    void read() {
+    void read() throws FileNotFoundException, IOException {
     
+        BufferedReader br = new BufferedReader(new FileReader("cache/configuration"));
+                
+        String string;
+        String text = null;
+        while ((string = br.readLine()) != null){
         
+            if(text != null){
+                text = text + string + "\n";
+            }else{
+                text = string + "\n";
+            }
+        }        
+        System.out.println(text);
+        
+        String[] textSplit = text.split("\n");
+        
+        System.out.println("PS07" + textSplit[0]);
+        System.out.println("VD01" + textSplit[1]);
         
     }
     
