ef16822c9affedfc0e3568d73d320c380d3ae5e8
DovgaN_Nik <nikita.dovgan.05@hotmail.com>
2021-08-14 23:57:43 +0300

    Added code for writing new configuration to file

--- src/jvesseltracker/Configuration.java
+++ src/jvesseltracker/Configuration.java
@@ -3,6 +3,7 @@ package jvesseltracker;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Configuration {
@@ -33,6 +34,17 @@ public class Configuration {
         System.out.println("VD01" + textSplit[1]);
         apiVD01 = textSplit[1];
         
+    }
+    
+    void write () throws IOException{
+        
+        String file = apiPS07 + "\n" + apiVD01;
+        
+        System.out.println("=== \n" + "start of file to be written \n" + file + "\n end" + "===");
+        
+        FileWriter fw = new FileWriter("cache/configuration");
+        fw.write(file);
+        fw.close();
     }
     
 }
