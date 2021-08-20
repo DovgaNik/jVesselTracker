86ca70812ce08a8ef36d721ffaea8d306221e39b
DovgaNik <nikita.dovgan.05@hotmail.com>
2021-08-20 16:00:49 +0300

    Full refactoring in JGet

--- src/jvesseltracker/webRequests/JGet.java
+++ src/jvesseltracker/webRequests/JGet.java
@@ -4,53 +4,27 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class JGet {
     
-    public static String download(String url){
-     
-        URL u;
-        InputStream is = null;
-        DataInputStream dis;
-        String s;
+    public static String download(URL url) throws IOException{
         
+        InputStream inputStream;
+        DataInputStream dataInputStream;
+        String string;
+
         String returning = "";
 
-        try {
-            
-            u = new URL(url);
-            is = u.openStream();
-            dis = new DataInputStream(new BufferedInputStream(is));
-            while ((s = dis.readLine()) != null)
-                returning += s;
-            
-        }
-        catch (MalformedURLException mue) {
-            
-            System.err.println("Ouch - a MalformedURLException happened.");
-            mue.printStackTrace();
-            
-        }
-        catch (IOException ioe) {
-            
-            System.err.println("Oops- an IOException happened.");
-            ioe.printStackTrace();
-            
-        }
-        finally {
-            
-          try {
-            is.close();
-          }
-              
-          catch (IOException ioe){}
-          
-        }
-        
+        inputStream = url.openStream();
+        dataInputStream = new DataInputStream(new BufferedInputStream(inputStream));
+        while ((string = dataInputStream.readLine()) != null)
+        returning += string;
+
+        inputStream.close();
+
         return returning;
-        
+
     }
     
 }
